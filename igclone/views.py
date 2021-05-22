c7ad88940c5f243d99a7de8b9598da68e1507d8e
mahan-noor <ismahanabey@gmail.com>
2021-05-23 00:45:53 +0300

    add function that fetches all images from users

--- igclone/views.py
+++ igclone/views.py
@@ -1,4 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+def index(request):
+	'''
+	Method that fetches all images from all users.
+	'''
+	images = Image.objects.all()
+	title = "Discover"
+	
+	return render(request,'index.html',{"images":images,"title":title})
