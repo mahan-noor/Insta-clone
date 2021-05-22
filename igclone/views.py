df88c08b9ebdac4030161a7b4a8e4d4a85dd170a
mahan-noor <ismahanabey@gmail.com>
2021-05-23 00:55:48 +0300

    add path pattern and create a method that fetch users profile page

--- igclone/views.py
+++ igclone/views.py
@@ -11,3 +11,25 @@ def index(request):
 	title = "Discover"
 	
 	return render(request,'index.html',{"images":images,"title":title})
+
+@login_required(login_url='/accounts/login/')
+def profile(request,prof_id):
+	'''
+	Method that fetches a users profile page
+	'''
+	user=User.objects.get(pk=prof_id)
+	images = Image.objects.filter(profile = prof_id)
+	title = User.objects.get(pk = prof_id).username
+	profile = Profile.objects.filter(user = prof_id)
+
+	if Follow.objects.filter(user_from=request.user,user_to = user).exists():
+		is_follow = True
+	else:
+		is_follow = False
+
+	followers = Follow.objects.filter(user_to = user).count()
+	following = Follow.objects.filter(user_from = user).count()
+	
+
+	return render(request,'accounts/profile.html',{"images":images,"profile":profile,"title":title,"is_follow":is_follow,"followers":followers,"following":following})
+	
