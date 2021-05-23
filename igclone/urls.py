d610a2eb209d647fc836d9c1e5b9c4d3a919d833
mahan-noor <ismahanabey@gmail.com>
2021-05-23 23:26:16 +0300

    setup paths for creating and updating profile

--- igclone/urls.py
+++ igclone/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns=[
     path('',views.index,name = 'index'),
     re_path('accounts/profile/(\d+)',views.profile,name = 'profile'),
+    path('accounts/create',views.create,name = 'create'),
+    path('accounts/updateProfile',views.updateProfile,name = 'updateProfile'),
+
+
 
 
 
