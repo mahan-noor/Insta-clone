d7c7a1b1242835cd754fa6eb54c49dcd7784cc36
mahan-noor <ismahanabey@gmail.com>
2021-05-24 15:26:21 +0300

    migrate and import class models in views and add class image follow and commment in models

--- igclone/views.py
+++ igclone/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from .models import Profile,Image,Comment,Follow
+
 # Create your views here.
 
 def index(request):
