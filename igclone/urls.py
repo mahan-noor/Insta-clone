37cf76fbbc6b89561c6615715cc64c1b84363d76
mahan-noor <ismahanabey@gmail.com>
2021-05-25 22:09:42 +0300

    drop the database and migrate again to fix the bug in url path

--- igclone/urls.py
+++ igclone/urls.py
@@ -4,15 +4,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    path('',views.index,name = 'index'),
-    re_path('accounts/profile/(\d+)',views.profile,name = 'profile'),
-    path('accounts/create',views.create,name = 'create'),
-    path('accounts/updateProfile',views.updateProfile,name = 'updateProfile'),
-    path('accounts/search',views.search,name = 'search'),
-    re_path('accounts/single/(\d+)',views.single,name = 'single'),
-    re_path('like/(\d+)',views.likePost,name= 'likePost'),
-    re_path('follow/(\d+)',views.follow,name="user_follow"),
-    re_path('editPost/(\d+)',views.editPost,name="editPost"),
+    path('',views.home,name = 'home'),
+    path('email/',views.email,name = 'email'),
+    path('create_profile/',views.create_profile,name = 'create_profile'),
+    re_path('profile/(?P<profile_id>\d+)',views.profile,name = 'profile'),
+    path('upload/image', views.upload_image, name = "upload_image"),
+    path('search/',views.search,name ='search'),
+    re_path('like/(?P<image_id>\d+)', views.like_image, name = 'like_image'),
+    re_path('comment/(?P<image_id>\d+)', views.comment,name = "comment"),
+    path('profile/edit', views.profile_edit,name = 'profile_edit'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
